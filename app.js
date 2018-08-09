cdf45624d2f9b738064c748f87522ec7d0eb49bc
Rjkirkhope <rjkirkhope@gmail.com>
2018-08-09 13:52:33 -0600

    Added Cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 const app = express();
 
 const routes = require("./restaurant_list");
 
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use("/restaurant_list", routes);
 
